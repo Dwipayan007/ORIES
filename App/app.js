c75ca2779e923b59084e8b4a31570c83c8cd807f
dwi.tripathy@gmail.com <dwi.tripathy@gmail.com>
2016-12-28 17:08:30 +0530

    Angularjs Addedand grunt added

---
+++ App/app.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="F:\Typescript\Ores\Ores\Scripts/angular-route.js" />
+/// <reference path="F:\Typescript\Ores\Ores\Scripts/angular.js" />
+var ores = angular.module("ores", ["ngRoute"]);
+ores.config(function ($routeProvider) {
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "App/Views/home.html"
+    }).otherwise({ redirectTo: "/home" });
+});
