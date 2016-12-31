ec7f96513e759d59a28bdcedaf2aeb8bf9db2a20
dwi.tripathy@gmail.com <dwi.tripathy@gmail.com>
2016-12-31 09:04:22 +0530

    Dynamic route added

--- App/Controllers/indexController.js
+++ App/Controllers/indexController.js
@@ -1,6 +1,9 @@
 ﻿/// <reference path="../app.js" />
-ores.controller("indexController", ["$scope", "mobileCheck", function ($scope, mobileCheck) {
+ores.controller("indexController", ["$scope", "$http", "$location","mobileCheck", function ($scope,$http, $location, mobileCheck) {
     if (mobileCheck) {
         angular.element(document.querySelector("#bs-example-navbar-collapse-2")).removeClass("banner-right")
+    }
+    $scope.fnGoToPage = function (args) {
+        $location.path('/' + args);
     }
 }])
