410c7c420ad2e1387c6c8f262bb73fa9dc86e35f
dwi.tripathy@gmail.com <dwi.tripathy@gmail.com>
2016-12-31 18:54:08 +0530

    About added and left nav bar added

---
+++ App/Controllers/aboutController.js
@@ -0,0 +1,8 @@
+﻿ores.controller("aboutController", ["$scope", "$http", "$location", "mobileCheck", function ($scope, $http, $location, mobileCheck) {
+    //if (mobileCheck) {
+    //    angular.element(document.querySelector("#bs-example-navbar-collapse-2")).removeClass("banner-right")
+    //}
+    //$scope.fnGoToPage = function (args) {
+    //    $location.path('/' + args);
+    //}
+}])
